11d7b91e342dc55122b62af9ae3f9ea3de06e830
Samuel Gonzalez <samglz10@gmail.com>
2024-05-22 21:17:49 -0400

    on mouse color working

--- Etch-A-Sketch/index.js
+++ Etch-A-Sketch/index.js
@@ -1,28 +1,39 @@
-console.log('hello world');
+
+//colors
+let defaultSizeGrid = 255;
 const container = document.getElementById('container');
 const rowOfDivs = document.createElement('div');
+const colors = document.querySelectorAll('.color');
+const gridSize = document.getElementById('grid-count');
+const gridSizeBtn =document.getElementById('grid-btn');
+
+gridSizeBtn.addEventListener('click', (e)=>{
+    defaultSizeGrid = e.target.value;
+    createCanvass(defaultSizeGrid);
+})
+console.log(defaultSizeGrid)
+//console.log(colors)
 //const getDiv = document.getElementById()
 
 
-for(let row = 0; row < 255; row++){
-    const rowOfDivs = document.createElement('div');
-    rowOfDivs.className = 'row';
-    rowOfDivs.id = row;
-    container.append(rowOfDivs);
-
-    rowOfDivs.addEventListener('click', (e)=>{
-        rowOfDivs.style.backgroundColor = 'red';
-        console.log(e.target.id);
-    })
+const createCanvass =(gridSize)=>{
+    for(let row = 0; row < gridSize; row++){
+        const rowOfDivs = document.createElement('div');
+        rowOfDivs.className = 'row';
+        rowOfDivs.id = row;
+        container.append(rowOfDivs);
+    
+        for(let i = 0; i < colors.length; i++){
+            let selectedColor = colors[i];
+            selectedColor.addEventListener('click',(e)=>{
+                let currentColor = e.target.value
+                rowOfDivs.addEventListener('mouseover', (e)=>{
+                    rowOfDivs.style.backgroundColor = currentColor;
+                })
+            })
+        }
+    }
 }
+createCanvass(defaultSizeGrid);
 
-
-
-/*
-for(let column = 0; column < 16; column++){
-    const rowOfDivs = document.createElement('div');
-    rowOfDivs.className = 'column';
-    rowOfDivs.innerText = column;
-    container.append(rowOfDivs);
-}*/
 
